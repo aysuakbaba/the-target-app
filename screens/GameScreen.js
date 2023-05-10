941294bb9db73c9cdd736d49c66090c4b0b85f8f
aysuakbaba <aysuakbabaa@gmail.com>
2023-05-10 15:07:13 +0300

    feat(title): seperated title component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,22 +1,22 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from "react-native";
+import Title from "../components/Title";
 function GameScreen() {
-    return(
-        <View style={styles.screen}>
-            <Text>Opponent's Guess</Text>
-            <View>
-                <Text>Higher or lower?</Text>
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
 
-            </View>
-        </View>
-    )
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+    </View>
+  );
 }
 
-
-export default GameScreen
+export default GameScreen;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        padding: 12
-    }
-})
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
